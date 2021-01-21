d4f0d47bb6c6283b89332359227ac2b162b366d2
Kevin <kevin_ljeong@hotmail.com>
2021-01-21 15:10:22 -0800

    cleaning up

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './Logo.png'
+import logo from './images/Logo.png'
 import { Link } from 'react-router-dom';
 
 const LogIn = ({ prescription_status, onPrescriptionOrder }) =>{
